b772c65d5f20718c062b3e28cf8df3555baacb77
Vector-ops <sumith2827@gmail.com>
2023-12-07 17:47:36 +0530

    feat: add favourites page

--- client/src/pages/Favorites.jsx
+++ client/src/pages/Favorites.jsx
@@ -1,7 +1,19 @@
-import React from "react";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { useLoaderData } from "react-router-dom";
+import SearchComponent from "../components/search/SearchComponent";
 
 const Favorites = () => {
-	return <div>Favorites</div>;
+	// const [bookmarks, setBookmarks] = useState([]);
+	// const [loading, setLoading] = useState(false);
+
+	const books = useLoaderData();
+
+	return (
+		<div className="app__favoutites">
+			<SearchComponent data={books} bookmark={true} />
+		</div>
+	);
 };
 
 export default Favorites;
