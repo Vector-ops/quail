312c475c66b5cfa568264c1bc26397fb4be0636b
Vector-ops <sumith2827@gmail.com>
2023-12-07 22:25:15 +0530

    Remove unnecessary imports and add new routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import {
 	Route,
 	RouterProvider,
@@ -6,10 +5,21 @@ import {
 	createRoutesFromElements,
 } from "react-router-dom";
 import "./App.scss";
+
+// Layouts
 import RootLayout from "./layouts/RootLayout";
+
+// Components
+import Success from "./components/success/Success";
+
+// Loaders
+import { favouritesLoader } from "./loaders/favouritesLoader";
 import { homePageLoader } from "./loaders/homePageLoader";
+
+// Pages
 import Catalog from "./pages/Catalog";
 import ErrorPage from "./pages/ErrorPage/ErrorPage";
+import Favourites from "./pages/Favorites";
 import Home from "./pages/Home";
 import LoginPage from "./pages/LoginPage/LoginPage";
 import NotFound from "./pages/NotFound/NotFound";
@@ -25,9 +35,15 @@ const router = createBrowserRouter(
 				errorElement={<ErrorPage />}
 			>
 				<Route index element={<Home />} loader={homePageLoader} />
-				<Route path="cart" element={<Catalog />} />
 				<Route path="book/:bookId" element={<ProductDetail />} />
 				<Route path="search/:search" element={<Catalog />} />
+				<Route
+					path="favourites"
+					element={<Favourites />}
+					loader={favouritesLoader}
+				/>
+				<Route path="favourites/:search" element={<Favourites />} />
+				<Route path="success" element={<Success />} />
 			</Route>
 			<Route path="/auth" errorElement={<ErrorPage />}>
 				<Route path="login" element={<LoginPage />} />
