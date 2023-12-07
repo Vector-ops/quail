ec938d48d0bcdc029b93d7c230528d77fff4ff63
Vector-ops <sumith2827@gmail.com>
2023-12-07 17:46:19 +0530

    feat: add bookmark

--- client/src/components/search/SearchComponent.jsx
+++ client/src/components/search/SearchComponent.jsx
@@ -1,32 +1,62 @@
+import { X } from "@phosphor-icons/react";
 import axios from "axios";
 import { useEffect, useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import BookOpen from "../../assets/BookOpen.png";
 import BookComponent from "../book/BookComponent";
 import DropDown from "../dropdown/DropDown";
 import SearchBar from "../searchBar/SearchBar";
 import "./SearchComponent.scss";
 
-const SearchComponent = ({ data, categories }) => {
+const SearchComponent = ({ data, categories, bookmark }) => {
 	const [loading, setLoading] = useState(false);
 	const [books, setBooks] = useState([]);
+	const [isSearch, setIsSearch] = useState(true);
 	const navigate = useNavigate();
+
+	let search = useParams().search;
+
+	search = search === undefined ? "" : search;
+
+	useEffect(() => {
+		if (data && data.length > 0) {
+			setBooks(data);
+		}
+	}, []);
+
+	bookmark = bookmark === null ? false : bookmark;
 
 	const filter = ["Price", "Rating", "Popularity"];
 
 	const handleSearch = (text) => {
-		if (text === "") {
+		if (text === "" || text === undefined) {
 			return;
 		}
-		navigate(`/search/${text}`);
+		navigate(`/${bookmark ? "favourites" : "search"}/${text}`);
+	};
+
+	const clearSearch = () => {
+		setIsSearch(false);
+		if (bookmark) {
+			window.location.href = `/favourites`;
+		}
 	};
 
 	useEffect(() => {
+		if (search === undefined || search === "") {
+			return;
+		}
+
 		setLoading(true);
 		const fetchResults = async () => {
 			try {
 				const response = await axios.get(
-					`http://localhost:3000/api/books/search/${data}`
+					`http://localhost:3000/api/books/${
+						bookmark ? "bookmarks" : "search"
+					}/${search}`,
+					{
+						withCredentials: true,
+					}
 				);
 				if (!response.data) {
 					throw new Error("Search results not found");
@@ -37,17 +67,29 @@ const SearchComponent = ({ data, categories }) => {
 				setBooks([]);
 			} finally {
 				setLoading(false);
+				setIsSearch(true);
 			}
 		};
 		fetchResults();
-	}, [data]);
+	}, [search]);
 
 	return (
 		<div className="app__search">
-			<SearchBar handleClick={handleSearch} />
+			<div className="app__search-bar">
+				<SearchBar handleClick={handleSearch} />
+				<X
+					size={32}
+					className="app__search-bar-clear"
+					onClick={() => clearSearch()}
+				/>
+			</div>
 			<div className="app__search-header">
 				<h2 className="search-text">
-					Results "<span>{data}</span>"
+					{isSearch && (
+						<>
+							Results "<span>{search}</span>"
+						</>
+					)}
 				</h2>
 				<div className="app__search-header-dropdown">
 					<DropDown variant="category" list={categories} />
@@ -60,7 +102,11 @@ const SearchComponent = ({ data, categories }) => {
 				) : books.length === 0 ? (
 					<div className="notFound-text">
 						<img src={BookOpen} alt="Not Found" />
-						<h1>Nothing was found :\</h1>
+						{bookmark ? (
+							<h1>Favourites will appear here :) </h1>
+						) : (
+							<h1>Nothing was found :\</h1>
+						)}
 					</div>
 				) : (
 					books.map((book, index) => {
